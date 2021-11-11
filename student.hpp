20f3873de3213fc3db7e505e1215389570616e49
RodrigoDiasUff <92749790+RodrigoDiasUff@users.noreply.github.com>
2021-11-11 20:51:08 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -10,9 +10,7 @@ int mul(int a, int b);
 int main(){
     int num1;
     int num2;
-    
-    std::cin >> num1;
-    std::cin >> num2;
+  
     add(num1, num2);
     mul(num1, num2);
 }
@@ -22,13 +20,13 @@ int main(){
 
 int add(int a, int b)
 {
-   std::cout << a + b << std::endl;
-   return 0; // TODO: fix
+ 
+   return a + b; // TODO: fix
 }
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
 {
-   std::cout << a * b << std::endl;
-   return 0; // TODO: fix
+   
+   return a * b; // TODO: fix
 }
